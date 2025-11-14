5eb5e44af0ce3ac7a8f218e709e6e661d3043cf3
Shubham Rawat <shubhamb23efd@gmail.com>
2025-11-14 16:24:39 +0530

    added dropdown component with outside click functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,14 @@ function App() {
   return (
     <div className="w-full h-screen flex justify-center items-center ">
       <Dropdown
-        options={["Option 1", "Option 2", "Option 3"]}
-        onChange={(e) => console.log(e)}
+        options={[
+          { id: "1", label: "Option 1", value: "opt1" },
+          { id: "2", label: "Option 2", value: "opt2" },
+          { id: "3", label: "Option 3", value: "opt3" },
+          { id: "4", label: "Option 4", value: "opt4" },
+          { id: "5", label: "Option 5", value: "opt5" },
+          { id: "6", label: "Option 6", value: "opt6" },
+        ]}
       />
     </div>
   );
--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,69 +1,65 @@
-import { cn } from "../utils/Util";
-import { useState, useRef, useEffect } from "react";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import React, { useEffect, useRef, useState } from "react";
 
+interface DropdownItem {
+  label: string;
+  value: string;
+  id: string;
+}
 interface DropdownProps {
-  options: string[];
-  onChange: (s: string) => void;
-  className?: string;
+  options: DropdownItem[];
 }
 
-const Dropdown = ({ options, onChange, className }: DropdownProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(options[0] || "");
+const Dropdown = ({ options }: DropdownProps) => {
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+  const [show, setShow] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
+      console.log(dropdownRef, event.target);
       if (
         dropdownRef.current &&
         !dropdownRef.current.contains(event.target as Node)
       ) {
-        setIsOpen(false);
+        setShow(false);
       }
     };
 
     document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
   }, []);
 
-  const handleOptionClick = (option: string) => {
+  const handleChange = (option: DropdownItem) => {
     setSelectedOption(option);
-    onChange(option);
-    setIsOpen(false);
+    setShow(false);
   };
 
   return (
-    <div
-      ref={dropdownRef}
-      className={cn("relative inline-block w-100", className)}
-    >
-      <button
-        type="button"
-        onClick={() => setIsOpen(!isOpen)}
-        className="w-full px-4 py-2 text-white bg-black rounded-md flex justify-between items-center"
+    <div className="relative w-100" ref={dropdownRef} tabIndex={0}>
+      <div
+        className="pl-4 pr-1 py-2 rounded-md w-full border border-black  flex cursor-pointer
+      gap-3 justify-between"
+        onClick={() => setShow(!show)}
       >
-        <span>{selectedOption}</span>
-        <span
-          className={cn(
-            "transition-transform duration-200",
-            isOpen ? "rotate-180" : ""
-          )}
-        >
-          ▼
-        </span>
-      </button>
-
-      {isOpen && (
-        <div className="absolute z-10 w-full mt-1 bg-white border border-gray-200 rounded-md shadow-lg">
-          {options.map((option, index) => (
-            <div
-              key={index}
-              onClick={() => handleOptionClick(option)}
-              className="px-4 py-2 cursor-pointer hover:bg-gray-100 text-gray-800 first:rounded-t-md last:rounded-b-md"
-            >
-              {option}
-            </div>
-          ))}
+        {selectedOption.label}
+        <span>{show ? <ChevronUp /> : <ChevronDown />}</span>
+      </div>
+      {show && (
+        <div className="absolute w-full border border-gray-300 rounded-sm">
+          {options.map((option) => {
+            return (
+              <div
+                className="hover:bg-gray-300  px-4 py-2 rounded-sm mt-1 cursor-pointer"
+                key={option.value}
+                onClick={() => handleChange(option)}
+              >
+                {option.label}
+              </div>
+            );
+          })}
         </div>
       )}
     </div>
